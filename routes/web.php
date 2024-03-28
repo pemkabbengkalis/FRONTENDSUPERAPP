1bb05133299e0ed5cb32b6153b231ee558aa6731
pendekarcoding <bohatimulyadi99@gmail.com>
2024-03-28 11:15:51 +0700

    update main route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.home');
-});
-
-Route::get('/home2', function () {
     return view('pages.home2');
 });
